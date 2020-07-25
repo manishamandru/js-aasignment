d99defb6474b6bbc1ba8361dd244a12e99afdd57
MANISHA MANDRU <manishamandru@gmail.com>
2020-07-25 22:59:31 +0530

    DAY 4

---
+++ DAY4 Q2.js
@@ -0,0 +1,10 @@
+onst student ={
+	 name:"Helsinki",
+	 age:24,
+	 projects:{
+		diceGame:"Two Player dice game using java script"
+	 },
+
+}
+const {name ,age, projects} = student;
+console.log(`${name} ${age} ${projects.dicegame}`);
